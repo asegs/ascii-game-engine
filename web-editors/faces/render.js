dc80385b322a0298d02f8a3a0f498df3886d053a
asegs <ajs2290@g.rit.edu>
2021-11-22 16:25:22 -0500

    Better color system

--- web-editors/faces/render.js
+++ web-editors/faces/render.js
@@ -3,24 +3,8 @@ const height = 10;
 const xDim = 20;
 const yDim = 40;
 let styles = [];
-const colors = ["R","G","B"];
 let selected = 0;
 const pixels = new Array(height);
-
-const RGBToHex= (r,g,b)=> {
-    r = r.toString(16);
-    g = g.toString(16);
-    b = b.toString(16);
-
-    if (r.length === 1)
-        r = "0" + r;
-    if (g.length === 1)
-        g = "0" + g;
-    if (b.length === 1)
-        b = "0" + b;
-
-    return "#" + r + g + b;
-}
 
 const initGrid = (ctx) => {
     for (let i = 0;i < height;i++){
@@ -33,12 +17,19 @@ const initGrid = (ctx) => {
 }
 
 
-const drawRect = (ctx,idx) => {
-    if (idx.x >= width || idx.y >= height){
+const drawRect = (ctx,idx, allowUndo) => {
+    if (styles.length === 0 || idx.x >= width || idx.y >= height){
         return;
     }
     const color = styles[selected];
-    ctx.fillStyle = RGBToHex(color[0],color[1],color[2]);
+    if (selected === pixels[idx.y][idx.x] && allowUndo){
+        pixels[idx.y][idx.x] = -1;
+        ctx.fillStyle = "#FFF";
+
+    }else{
+        ctx.fillStyle = color;
+        pixels[idx.y][idx.x] = selected;
+    }
     ctx.fillRect(xDim * idx.x,yDim * idx.y,xDim,yDim);
 }
 
@@ -48,11 +39,10 @@ var c = document.getElementById("canvas");
 var rect = c.getBoundingClientRect();
 const ctx = c.getContext("2d");
 
-function printMousePos(event) {
+function handleClick(event) {
     const pos = getPos(event,rect);
     const idx = getTileIdx(pos);
-    drawRect(ctx,idx);
-    pixels[idx.y][idx.x] = selected;
+    drawRect(ctx,idx,true);
 }
 
 const getPos = (event,rect) => {
@@ -64,8 +54,8 @@ const getPos = (event,rect) => {
 
 const getTileIdx = (pos) => {
     const idx = {};
-    idx.x = parseInt(pos.x / xDim);
-    idx.y = parseInt(pos.y / yDim);
+    idx.x = Math.trunc(pos.x / xDim);
+    idx.y = Math.trunc(pos.y / yDim);
     return idx;
 }
 
@@ -77,71 +67,39 @@ const downloadString = (text) => {
     link.click();
 }
 document.getElementById("canvas").style.border = "thin dotted #000";
-c.addEventListener("click", printMousePos);
+c.addEventListener("click", handleClick);
 const controls = document.getElementById("controls")
 const addStyle = document.getElementById("add-style");
 addStyle.onclick = () => {
     const styleIdx = styles.length;
-    const colorDiv = document.createElement("div");
-    colorDiv.id = "color-div-" + styleIdx;
-    let ct = 0;
-    for (const color of colors){
-        const iC = document.createElement("input");
-        const label = document.createElement("label");
-        iC.type = "number";
-        iC.defaultValue = "0";
-        iC.min = "0";
-        iC.max = "255";
-        const tempPos = ct;
-        iC.onchange = (event) => {
-            const colorSlice = styles[styleIdx];
-            styles[styleIdx][tempPos] = parseInt(event.target.value);
-            const cBtn = document.getElementById("color-btn-"+styleIdx);
-            if (cBtn){
-             cBtn.style.backgroundColor = RGBToHex(colorSlice[0],colorSlice[1],colorSlice[2]);
-            }
-            for (let i = 0;i<height;i++){
-                for (let b = 0;b<width;b++){
-                    if (pixels[i][b] === styleIdx){
-                        drawRect(ctx,{x:b,y:i});
-                    }
+    const picker = document.createElement("input");
+    picker.type = "color";
+    picker.onclick = () => {
+        selected = styleIdx;
+    }
+    picker.onchange = (event) => {
+        styles[styleIdx] = event.target.value;
+        for (let i = 0;i<height;i++){
+            for (let b = 0;b<width;b++){
+                if (pixels[i][b] === styleIdx){
+                    drawRect(ctx,{x:b,y:i},false);
                 }
             }
         }
-        iC.id = color+"-input-"+styleIdx;
-        label.htmlFor = color+"-input-"+styleIdx;
-        label.innerText = color + ":";
-        colorDiv.append(label);
-        colorDiv.append(iC);
-        ct++;
     }
-    const colorBtn = document.createElement("button");
-    styles.push([0,0,0]);
-    colorBtn.style.backgroundColor = "#000";
-    colorBtn.id = "color-btn-" + styleIdx;
-    colorBtn.innerText = "EXAMPLE";
-    colorDiv.append(colorBtn);
-    colorDiv.onclick = () => {
-        selected = styleIdx;
-        for (let i = 0;i<styles.length;i++){
-            if (selected === i){
-                document.getElementById("color-div-" + i).style.backgroundColor = "#c2fcfc";
-            } else {
-                document.getElementById("color-div-" + i).style.backgroundColor = "#FFF";
-            }
-        }
 
-    }
-    controls.insertBefore(colorDiv,addStyle);
+    styles.push("#000");
+    controls.insertBefore(picker,addStyle);
+    controls.insertBefore(document.createElement("br"),addStyle);
 }
-initGrid(ctx,20);
+initGrid(ctx);
 const saveFace = document.getElementById("save");
 saveFace.onclick = () => {
     const body = new Array(width * height + height);
     let current = 0;
     for (let i = 0;i<height;i++){
         for (let b = 0;b<width;b++){
-            body[current] = pixels[i][b] ?? "-1";
+            body[current] = pixels[i][b];
             current++;
         }
         body[current] = "\n";
@@ -150,5 +108,5 @@ saveFace.onclick = () => {
     downloadString(body.join(","));
 }
 for (let i = 0 ; i < height ; i++ ){
-    pixels[i] = new Array(width);
+    pixels[i] = new Array(width).fill(-1);
 }
