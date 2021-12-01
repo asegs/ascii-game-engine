32b03f3349b7dc197acd47dc719011366a40a702
asegs <ajs2290@g.rit.edu>
2021-12-01 16:32:19 -0500

    Working on zoning rules

--- zones.go
+++ zones.go
@@ -1,9 +1,13 @@
 package main
 
+import "errors"
+
 type Zoning struct {
 	Zones [] * Zone
 	Height int
 	Width int
+	CursorY int
+	CursorX int
 }
 
 type Zone struct {
@@ -22,8 +26,25 @@ func initZones (height int,width int) * Zoning{
 	}
 }
 
+func (z * Zoning) zonesIntersect (a * Zone, b * Zone) bool {
+	return (a.X <= b.X + b.Width) && (a.X + a.Width >= b.X) && (a.Y <= b.Y + b.Height) && (a.Y + a.Height >= b.Y)
+}
+
 func (z * Zoning) createObject (Y int, X int, Height int, Width int, CursorAllowed bool) error {
-	//check if zone mapping is good, if shape is within other zone
+	newZone := &Zone{
+		Y:             Y,
+		X:             X,
+		Height:        Height,
+		Width:         Width,
+		CursorAllowed: CursorAllowed,
+	}
+	for _,zone := range z.Zones {
+		if z.zonesIntersect(zone,newZone){
+			//specify which zone
+			return errors.New("zone intersects with preexisting zone")
+		}
+	}
+	z.Zones = append(z.Zones,newZone)
 	return nil
 }
 
