518c96df989aef68a606177f57a55ba97d2ef866
Aaron Segal <ajs2290@g.rit.edu>
2022-02-19 18:01:56 -0500

    Basic audio functions

--- audio.go
+++ audio.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"os/exec"
+)
+
+/**
+This is currently built using FFPlay, provided by FFMpeg.
+Right now, the reason is that this project is to be free of dependencies.
+However, this may be difficult to handle on every OS, so it may end up using a library for sound.
+
+If necessary, Go supports cancelling processes after any amount of time via callback,
+so we could easily add a "playFor" function.
+ */
+
+type Audio struct {
+	Filename string
+	Handle * exec.Cmd
+}
+
+func play(filename string) (error, * Audio ) {
+	cmd := exec.Command("ffplay","nodisp","autoexit",filename)
+	err := cmd.Start()
+	return err,&Audio{Filename: filename,Handle: cmd}
+}
+
+func (a * Audio) stop() error {
+	err := a.Handle.Process.Kill()
+	return err
+}
+
--- faces.go
+++ faces.go
@@ -7,7 +7,12 @@ type Face struct {
 	Expressions map[string] * Frame
 }
 
-func (t * Terminal) playSingleExpression (face * Face,exp string,zone * Zone) {
+var interrupt = make(chan bool,3)
+
+func (t * Terminal) playSingleExpression (face * Face,exp string,zone * Zone,interruptCycles bool) {
+	if interruptCycles {
+		interrupt <- true
+	}
 	if frame, ok := face.Expressions[exp]; ok {
 		t.drawFrame(frame,zone.Y,zone.X)
 	}
@@ -15,16 +20,17 @@ func (t * Terminal) playSingleExpression (face * Face,exp string,zone * Zone) {
 
 //delay doesn't consider time to print, also cycles idea is weird, have a current state of face
 func (t * Terminal) cycleExpressions (face * Face, exps [] string, msDelay int,cycles int,zone * Zone){
-	for cycles == -1 || 0 < cycles {
+	for i := 0 ; i < cycles ; i ++ {
 		for _,exp := range exps {
-			t.playSingleExpression(face,exp,zone)
+			if len(interrupt) > 0 {
+				return
+			}
+			t.playSingleExpression(face,exp,zone,false)
 			time.Sleep(time.Millisecond * time.Duration(msDelay))
-		}
-		if cycles != -1 {
-			cycles --
 		}
 	}
 }
+
 
 func buildFace (exps [] string,filenames [] string,name string) * Face{
 	expMap := make(map[string] * Frame)
