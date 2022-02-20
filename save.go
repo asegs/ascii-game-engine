f7cd60f8625708b044c45e51e0001bea2f128fd0
Aaron Segal <ajs2290@g.rit.edu>
2022-02-20 10:07:57 -0500

    Some not fleshed out methods for saving a state

--- save.go
+++ save.go
@@ -1,9 +1,38 @@
 package main
 
-func (t * Terminal) saveState (filename string) error{
+/**
+If recorded is in provided list - record number in list, nothing more.
+If not, consider recorded erratas and store in file as JSON/something.
+ */
+
+func recordedEquals (r1 * Recorded, r2 * Recorded) bool {
+	return r1.Format.Format == r2.Format.Format &&
+		r1.BackgroundCode == r2.BackgroundCode &&
+		r1.ShownSymbol == r2.ShownSymbol
+}
+
+func recordedIndex (r * Recorded, styles [] * Recorded) int {
+	for i, recorded := range styles {
+		if recordedEquals(r,recorded) {
+			return i
+		}
+	}
+	return -1
+}
+
+func recordedToString(r * Recorded) {
 
 }
 
-func (t * Terminal) loadState (filename string) error {
+func recordedFromString (record string) * Recorded {
+
+}
+
+
+func (t * Terminal) saveState (filename string,styles [] * Recorded) error{
+
+}
+
+func (t * Terminal) loadState (filename string, styles [] * Recorded) error {
 
 }
