428edadf459755cf7fbcad0da447da93f40d8ed1
Aaron Segal <ajs2290@g.rit.edu>
2022-02-21 11:28:10 -0500

    Marshalling/unmarshalling recorded

--- save.go
+++ save.go
@@ -1,9 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+)
+
 /**
 If recorded is in provided list - record number in list, nothing more.
 If not, consider recorded erratas and store in file as JSON/something.
- */
+*/
 
 func recordedEquals (r1 * Recorded, r2 * Recorded) bool {
 	return r1.Format.Format == r2.Format.Format &&
@@ -20,12 +24,18 @@ func recordedIndex (r * Recorded, styles [] * Recorded) int {
 	return -1
 }
 
-func recordedToString(r * Recorded) {
-
+func recordedToString(r * Recorded) string{
+	output,err := json.Marshal(r)
+	if err != nil {
+		return ""
+	}
+	return string(output)
 }
 
-func recordedFromString (record string) * Recorded {
-
+func recordedFromString (record string) (error, * Recorded) {
+	var r Recorded
+	err := json.Unmarshal([]byte(record),&r)
+	return err,&r
 }
 
 
@@ -36,3 +46,20 @@ func (t * Terminal) saveState (filename string,styles [] * Recorded) error{
 func (t * Terminal) loadState (filename string, styles [] * Recorded) error {
 
 }
+
+//func main()  {
+//	mods := make([]string,2)
+//	mods[0] = "one"
+//	mods[1] = "two"
+//	r := &Recorded{
+//		Format:         &Context{
+//			Format:    "test",
+//			Modifiers: mods,
+//		},
+//		ShownSymbol:    'a',
+//		BackgroundCode: 'x',
+//	}
+//	s := recordedToString(r)
+//	_,r2 := recordedFromString(s)
+//	fmt.Println(r2.ShownSymbol)
+//}
