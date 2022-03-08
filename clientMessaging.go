daa0d36c0656cf3d39f2a85742e22a5f938b56af
asegs <ajs2290@g.rit.edu>
2022-03-07 20:17:19 -0500

    It's not Go, it's Javascriptgit add .

--- clientMessaging.go
+++ clientMessaging.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"reflect"
+	"time"
 )
 
 /**
@@ -12,10 +13,67 @@ Send key for state and new value with port/ID whenever state updates
 Do this with reflect for now and if it is too slow after profiling do some code generation before compile
  */
 
-func toStateUpdate(state interface{}, key string, id int) string {
-	field := reflect.ValueOf(state).FieldByName(key)
-	toJson,_ := json.Marshal(field.Interface())
-	return fmt.Sprintf("%d,%s,%v",id,key,string(toJson))
+type CoordExample struct {
+	X int
+	Y int
 }
 
+type StateExample struct {
+	Name string
+	Loc CoordExample
+}
+
+type UpdateMessage struct {
+	Id int
+	Key string
+	Value interface{}
+}
+
+func toStateUpdate(state interface{}, key string, id int) * UpdateMessage {
+	field := reflect.ValueOf(state).FieldByName(key)
+	return &UpdateMessage{
+		Id:    id,
+		Key:   key,
+		Value: field.Interface(),
+	}
+}
+
+func (u * UpdateMessage) toBytes() []byte{
+	output,_ := json.Marshal(u)
+	return output
+}
+
+func messageFromBytes (bytes []byte) * UpdateMessage {
+	var update UpdateMessage
+	_ = json.Unmarshal(bytes,&update)
+	return &update
+
+}
+
+func updateStateFromMessage(state interface{},message * UpdateMessage) {
+	reflect.Indirect(reflect.ValueOf(state)).FieldByName(message.Key).Set(reflect.ValueOf(message.Value))
+}
+
+func main()  {
+	state := StateExample{
+		Name: "Aaron",
+		Loc:  CoordExample{
+			X: 1,
+			Y: 2,
+		},
+	}
+	localState := StateExample{
+		Name: "Ronnie",
+		Loc:  CoordExample{
+			X: 2,
+			Y: 3,
+		},
+	}
+	start := time.Now()
+	update := toStateUpdate(state,"Name",0)
+	packet := update.toBytes()
+	received := messageFromBytes(packet)
+	updateStateFromMessage(&localState,received)
+	fmt.Println(time.Now().Sub(start))
+}
 
